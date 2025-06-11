e807b7ef45d6e7625378145f581b14a9c9dc8491
chyo1 <hyowon3856@gmail.com>
2025-06-11 21:18:27 +0900

    10. road display draft

---
+++ src/decorator/RoadDisplay.java
@@ -0,0 +1,8 @@
+package decorator;
+
+public class RoadDisplay {
+
+	public void draw() {
+		System.out.println("기본 도로 표시");
+	}
+}
---
+++ src/decorator/RoadDisplayWithTraffic.java
@@ -0,0 +1,14 @@
+package decorator;
+
+public class RoadDisplayWithTraffic extends RoadDisplay {
+
+	@Override
+	public void draw() {
+		super.draw();
+		drawTraffic();
+	}
+
+	private void drawTraffic() {
+		System.out.println("교통량 표시");
+	}
+}
