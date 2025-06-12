1ccce8b29cd67097906d83128447993789c82388
chyo1 <hyowon3856@gmail.com>
2025-06-12 15:35:19 +0900

    [FIX] 9. battery draft

---
+++ src/observer/battery/BatteryLevelDisplay.java
@@ -0,0 +1,15 @@
+package observer.battery;
+
+public class BatteryLevelDisplay {
+
+	private Battery battery;
+
+	public BatteryLevelDisplay(Battery battery) {
+		this.battery = battery;
+	}
+
+	public void update() {
+		int level = battery.getLevel();
+		System.out.println("level : " + level);
+	}
+}
